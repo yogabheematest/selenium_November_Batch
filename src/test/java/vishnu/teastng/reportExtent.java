16b6b62f51cafd7134ac4769cd5359696619553f
vishnu <vishuvardhannaidu999@gmail.com>
2022-12-12 10:50:38 +0530

    project is commited by Vishnu

---
+++ src/test/java/vishnu/teastng/reportExtent.java
@@ -0,0 +1,117 @@
+package vishnu.teastng;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class reportExtent {
+	
+	public WebDriver driver;
+	public ExtentSparkReporter spark;
+	public ExtentReports extent;
+	public ExtentTest logger;
+	
+	@BeforeTest
+	public void startReport() {
+	         // Create an object of Extent Reports
+	extent = new ExtentReports();
+	 
+	spark = new ExtentSparkReporter(System.getProperty("user.dir") 
+			+ "/test-output/STMExtentReport.html");
+	extent.attachReporter(spark);
+	extent.setSystemInfo("Host Name", "Yoganand");
+	         extent.setSystemInfo("Environment", "Production");
+	extent.setSystemInfo("User Name", "Yoganand");
+	spark.config().setDocumentTitle("QA Status Report ");
+	                // Name of the report
+	spark.config().setReportName("Daily Report");
+	                // Dark Theme
+	spark.config().setTheme(Theme.STANDARD);
+	}
+	
+	//This method is to capture the screenshot and return the path of the screenshot.
+	public static String getScreenShot(WebDriver driver,
+			String screenshotName) throws IOException 
+	{
+	String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+	TakesScreenshot ts = (TakesScreenshot) driver;
+	File source = ts.getScreenshotAs(OutputType.FILE);
+	// after execution, you could see a folder "FailedTestsScreenshots" under src folder
+	String destination = System.getProperty("user.dir") + "/Screenshots/" + screenshotName + dateName + ".png";
+	File finalDestination = new File(destination);
+	FileUtils.copyFile(source, finalDestination);
+	return destination;
+	}
+	@BeforeMethod
+	public void setup() {
+    System.setProperty("webdriver.chrome.driver","./driver_folder/chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.google.com/");
+	}
+	@Test
+	public void verifyTitle() {
+	logger = extent.createTest("To verify Google Title");
+	Assert.assertEquals(driver.getTitle(),"Google");
+	}
+	@Test
+	public void verifyLogo() {
+	logger = extent.createTest("To verify Google Logo");
+	boolean img = driver.findElement(By.xpath("//img&#91;@id='hplogo']")).isDisplayed();
+	logger.createNode("Image is Present");
+	Assert.assertTrue(img);
+	logger.createNode("Image is not Present");
+	Assert.assertFalse(img);
+	}
+	@AfterMethod
+	public void getResult(ITestResult result) throws Exception{
+	if(result.getStatus() == ITestResult.FAILURE){
+	//MarkupHelper is used to display the output in different colors
+	logger.log(Status.FAIL, MarkupHelper.createLabel(result.getName() + " - Test Case Failed", ExtentColor.RED));
+	logger.log(Status.FAIL, MarkupHelper.createLabel(result.getThrowable() + " - Test Case Failed", ExtentColor.RED));
+	//To capture screenshot path and store the path of the screenshot in the string "screenshotPath"
+	//We do pass the path captured by this method in to the extent reports using "logger.addScreenCapture" method.
+	//String Scrnshot=TakeScreenshot.captuerScreenshot(driver,"TestCaseFailed");
+	String screenshotPath = getScreenShot(driver, result.getName());
+	//To add it in the extent report
+	logger.fail("Test Case Failed Snapshot is below " + logger.addScreenCaptureFromPath(screenshotPath));
+	}
+	else if(result.getStatus() == ITestResult.SKIP){
+	logger.log(Status.SKIP, MarkupHelper.createLabel(result.getName() + " - Test Case Skipped", ExtentColor.ORANGE));
+	}
+	else if(result.getStatus() == ITestResult.SUCCESS)
+	{
+	logger.log(Status.PASS, MarkupHelper.createLabel(result.getName()+" Test Case PASSED", ExtentColor.GREEN));
+	}
+	driver.quit();
+	}
+	@AfterTest
+	public void endReport() {
+	extent.flush();
+	}
+
+}
+
+
